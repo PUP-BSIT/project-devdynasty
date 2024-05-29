48e9fa3bd200ebe03b3c3c1aa1a852fca44a6580
Jonnel Tocmo <jonneltocmo137@gmail.com>
2024-05-29 12:05:30 +0800

    fix issues

--- src/app/dashboard/job-list/job-list.component.ts
+++ src/app/dashboard/job-list/job-list.component.ts
@@ -7,11 +7,51 @@ import { Component } from '@angular/core';
 })
 export class JobListComponent {
   jobs = [
-    { title: 'SOFTWARE DEVELOPER', company: 'Microsoft Philippines', location: 'BGC, Philippines', salary: '$ 1,000', highlighted: false, icon: 'assets/Job_Page/globe_icon.png', salaryIcon: 'assets/Job_Page/money_icon.png' },
-    { title: 'SYSTEM ANALYST', company: 'DTI Philippines', location: 'Pasay City, Philippines', salary: '$ 1,000', highlighted: false, icon: 'assets/Job_Page/globe_icon.png', salaryIcon: 'assets/Job_Page/money_icon.png' },
-    { title: 'WEB DEVELOPER', company: 'Google Operations, USA', location: 'Taguig City, Philippines', salary: '$ 1,000', highlighted: true, icon: 'assets/Job_Page/globe_icon.png', salaryIcon: 'assets/Job_Page/money_icon.png' },
-    { title: 'SOFTWARE DEVELOPER', company: 'Microsoft Philippines', location: 'Pasay City, Philippines', salary: '$ 1,000', highlighted: false, icon: 'assets/Job_Page/globe_icon.png', salaryIcon: 'assets/Job_Page/money_icon.png' },
-    { title: 'SYSTEM ANALYST', company: 'DTI Philippines', location: 'Pasay City, Philippines', salary: '$ 1,000', highlighted: false, icon: 'assets/Job_Page/globe_icon.png', salaryIcon: 'assets/Job_Page/money_icon.png' }
+    {
+      title: 'SOFTWARE DEVELOPER', 
+      company: 'Microsoft Philippines', 
+      location: 'BGC, Philippines', 
+      salary: '$ 1,000', 
+      highlighted: false, 
+      icon: 'assets/Job_Page/globe_icon.png', 
+      salaryIcon: 'assets/Job_Page/money_icon.png'
+    },
+    {
+      title: 'SYSTEM ANALYST', 
+      company: 'DTI Philippines', 
+      location: 'Pasay City, Philippines', 
+      salary: '$ 1,000', 
+      highlighted: false, 
+      icon: 'assets/Job_Page/globe_icon.png', 
+      salaryIcon: 'assets/Job_Page/money_icon.png' 
+    },
+    {
+      title: 'WEB DEVELOPER', 
+      company: 'Google Operations, USA', 
+      location: 'Taguig City, Philippines', 
+      salary: '$ 1,000', 
+      highlighted: true, 
+      icon: 'assets/Job_Page/globe_icon.png', 
+      salaryIcon: 'assets/Job_Page/money_icon.png'
+    },
+    {
+      title: 'SOFTWARE DEVELOPER', 
+      company: 'Microsoft Philippines', 
+      location: 'Pasay City, Philippines', 
+      salary: '$ 1,000', 
+      highlighted: false, 
+      icon: 'assets/Job_Page/globe_icon.png', 
+      salaryIcon: 'assets/Job_Page/money_icon.png' 
+    },
+    {
+      title: 'SYSTEM ANALYST', 
+      company: 'DTI Philippines', 
+      location: 'Pasay City, Philippines', 
+      salary: '$ 1,000', 
+      highlighted: false, 
+      icon: 'assets/Job_Page/globe_icon.png', 
+      salaryIcon: 'assets/Job_Page/money_icon.png' 
+    }
   ];
   onApplyClick(event: Event) {
     const button = event.target as HTMLElement;
