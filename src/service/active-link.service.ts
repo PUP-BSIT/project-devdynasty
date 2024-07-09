9e76b425c056d5db4b297f8073f973f691ed35a9
Jonnel Tocmo <jonneltocmo137@gmail.com>
2024-07-09 22:29:01 +0800

    Enhance UI responsiveness

--- src/service/active-link.service.ts
+++ src/service/active-link.service.ts
@@ -1,7 +1,7 @@
 import { BehaviorSubject } from 'rxjs';
 
 export class ActiveLinkService {
-  private activeLinkSubject = new BehaviorSubject('home');
+  private activeLinkSubject = new BehaviorSubject<string>('home');
   activeLink$ = this.activeLinkSubject.asObservable();
 
   setActiveLink(link: string) {
